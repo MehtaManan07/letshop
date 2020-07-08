b6019524fc200ed24077406ece7da18a20247d5c
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-07-09 02:21:50 +0530

    Category model

---
+++ models/Category.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const categorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: true,
+    maxlength: 32,
+  },
+},{ timestamps: true });
+module.exports = Category = mongoose.model("Category", categorySchema);
