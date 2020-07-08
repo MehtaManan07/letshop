32fd5444ac07351c346939057831db1896d764c4
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-07-09 02:58:09 +0530

    Product model

---
+++ models/Product.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 32,
+    },
+    description: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 3000,
+    },
+    price: {
+      type: Number,
+      trim: true,
+      required: true,
+      maxlength: 15,
+    },
+    Category: {
+      type: ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    quantity: {
+      type: Number,
+    },
+    picture: {
+      data: Buffer,
+      contentType: String,
+    },
+    shipping: {
+        required: false,
+        type: Boolean
+    }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = Product = mongoose.model("Product", productSchema);
