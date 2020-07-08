62601ddd8a9db7206032020d6e4a4629b4f4e26b
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-07-09 05:22:50 +0530

    All crud operations on categories

--- controllers/Category.js
+++ controllers/Category.js
@@ -3,19 +3,14 @@ const Category = require("../models/Category");
 exports.categoryById = (req, res, next, id) => {
   Category.findById(id).exec((error, category) => {
     if (error || !category) {
-      return res.json({ error: `category not found` });
+      return res.status(400).json({ error: `category not found` });
     }
     req.category = category;
     next();
   });
 };
 
-exports.getCategoryById = (req, res) => {
- return res.json(req.category);
-};
-
-
-exports.create = (req, res, next) => {
+exports.create = (req, res) => {
   const newCategory = new Category(req.body);
   newCategory.save((error, data) => {
     if (error) {
@@ -23,5 +18,45 @@ exports.create = (req, res, next) => {
     }
 
     res.status(201).json({ data });
+  });
+};
+
+exports.getCategoryById = (req, res) => {
+  return res.json(req.category);
+};
+
+exports.getAllCategories = (req, res) => {
+  Category.find().exec((error, data) => {
+    // console.log(typeof data);
+    if (error) {
+      res.status(400).json({ error: "No data found" });
+    } 
+    res.json(data)
+  });
+};
+
+
+exports.updateCategoryById = (req, res) => {
+  const category = req.category;
+  category.name = req.body.name;
+  category.save((error, updatedCategory) => {
+    if (error) {
+      return res.status(400).json({
+        error: "Category cannot not be updated",
+      });
+    }
+    res.json({ updatedCategory });
+  });
+};
+
+exports.deleteCategoryById = (req, res) => {
+  const category = req.category;
+  category.save((error, deletedCategory) => {
+    if (error) {
+      return res.status(400).json({
+        error: "Category cannot not be deleted",
+      });
+    }
+    res.json({ deletedCategory });
   });
 };
--- routes/Category.js
+++ routes/Category.js
@@ -1,14 +1,29 @@
 const express = require("express");
 const router = express.Router();
 
-const { create, categoryById, getCategoryById } = require("../controllers/Category");
+const { create, categoryById, getCategoryById, updateCategoryById, getAllCategories, deleteCategoryById } = require("../controllers/Category");
 const { requireLogin, isAdmin, isAuth } = require("../controllers/Auth");
 const { userById } = require("../controllers/User");
 
+router.get('/', getAllCategories)
 router.get('/:categoryId', getCategoryById)
 router.post("/create/:userId", requireLogin, isAuth, isAdmin, create);
+router.delete(
+    "/:categoryId/:userId",
+    requireLogin,
+    isAdmin,
+    isAuth,
+    deleteCategoryById
+  );
+  router.put(
+    "/:categoryId/:userId",
+    requireLogin,
+    isAdmin,
+    isAuth,
+    updateCategoryById
+  );
 
 router.param('userId', userById)
-router.param('categoryId', getCategoryById)
+router.param('categoryId', categoryById)
 
 module.exports = router
