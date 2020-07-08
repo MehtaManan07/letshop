d5c45904a9def02ec91735fce51a93249ade404b
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-07-08 16:53:28 +0530

    User schema methods to hash password

--- models/Users.js
+++ models/Users.js
@@ -1,38 +1,80 @@
 const mongoose = require("mongoose");
 const crypto = require("crypto");
+const uuidv1 = require("uuid/v1");
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: true,
-    maxlength: 32,
-  },
-  email: {
-    type: String,
-    trim: true,
-    required: true,
-    unique: true,
-  },
-  hashed_password: {
-    type: String,
-    trim: true,
-    required: true,
-    maxlength: 32,
-  },
-  about: {
-    type: String,
-    trim: true,
-  },
-  salt: String,
-  role: {
-    type: Number,
-    default: 0, // normal users - 0 and admin - 1
-  },
-  history: {
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 32,
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+    hashed_password: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 32,
+    },
+    about: {
+      type: String,
+      trim: true,
+    },
+    salt: String,
+    role: {
+      type: Number,
+      default: 0, // normal users - 0 and admin - 1
+    },
+    history: {
       type: Array,
-      default: []
-  }
-}, {
+      default: [],
+    },
+  },
+  {
     timestamps: true,
-});
+  }
+);
+
+userSchema
+  .virtual("password")
+  .set(function (password) {
+    // create a temporarity variable called _password
+    this._password = password;
+    // generate salt
+    this.salt = uuidv1();
+    // encryptPassword
+    this.hashed_password = this.encryptPassword(password);
+  })
+  .get(function () {
+    return this._password;
+  });
+
+userSchema.methods = {
+  authenticate: function (plainText) {
+    return this.encryptPassword(plainText) === this.hashed_password;
+  },
+
+  encryptPassword: function (password) {
+    if (!password) return "";
+    try {
+      return crypto
+        .createHmac("sha1", this.salt)
+        .update(password)
+        .digest("hex");
+    } catch (err) {
+      return "";
+    }
+  },
+
+  makeSalt: function () {
+    return Math.round(new Date().valueOf() * Math.random()) + "";
+  },
+};
+
+module.exports = User = mongoose.model("User", userSchema);
