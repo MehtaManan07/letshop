886cc9afeafdc2dab2d3fa739d4cecd9908d7702
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-07-08 15:18:45 +0530

    basic setup and database connected

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const morgan = require('morgan');
+const colors = require('colors');
+const mongoose = require('mongoose');
+require('dotenv').config()
+const app = express();
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+},() => {
+    console.log(`mongo connected`.bgGreen)
+});
+
+// Body parser
+app.use(express.json())
+
+// Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
+  }
+
+  //Routes
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`app running at http://localhost:${port}/`.bgBlue)
+})
