7484945557b53d75c458ada319d684cec8bcb5b3
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-07-10 03:55:41 +0530

    Navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -11,36 +11,41 @@ const isActive = (history, path) => {
 
 const Navbar = (props) => {
   return (
-    <nav className="navbar navbar-dark bg-dark navbar-expand-sm fixed-top">
-      <div className="container">
-        <Link
-          to="/"
-          style={isActive(props.history, "/")}
-          className="navbar-brand"
-        >
-          Let's Shop
-        </Link>
-        <div className="collapse navbar-collapse">
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <Link
-                to="/login"
-                className="nav-link"
-                style={isActive(props.history, "/login")}
-              >
-                Login
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                to="/register"
-                className="nav-link"
-                style={isActive(props.history, "/register")}
-              >
-                Register
-              </Link>
-            </li>
-          </ul>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <Link
+        to="/"
+        style={isActive(props.history, "/")}
+        className="navbar-brand"
+      >
+        Let's Shop
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="ml-auto navbar-nav">
+          <Link
+            className="nav-item nav-link"
+            style={isActive(props.history, "/login")}
+            to="/login"
+          >
+            Login
+          </Link>
+          <Link
+            className="nav-item nav-link"
+            style={isActive(props.history, "/register")}
+            to="/register"
+          >
+            Register
+          </Link>
         </div>
       </div>
     </nav>
