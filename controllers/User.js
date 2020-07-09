dfe26b1dd12c2d2c8ca0dbe3cfadf356ea40c1dd
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-07-09 21:35:59 +0530

    Get and update profile

--- controllers/User.js
+++ controllers/User.js
@@ -9,3 +9,26 @@ exports.userById = (req, res, next, id) => {
     next();
   });
 };
+
+exports.getProfile = (req, res) => {
+  req.profile.hashed_password = undefined;
+  req.profile.salt = undefined;
+
+  return res.json(req.profile);
+};
+
+exports.updateProfile = (req, res) => {
+  User.findOneAndUpdate(
+    { _id: req.profile._id },
+    { $set: req.body },
+    { new: true },
+    (error, user) => {
+      if(error) {
+        return res.status(400).json({ error: 'Unauthorized to perform these actions' })
+      }
+      user.hashed_password = undefined;
+      user.salt = undefined;
+      res.json({ user})
+    }
+  );
+};
--- routes/User.js
+++ routes/User.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { userById } = require('../controllers/User');
+const { userById, updateProfile, getProfile } = require('../controllers/User');
 const { requireLogin, isAuth } = require('../controllers/Auth')
 
 router.get('/secret/:userId', requireLogin, isAuth, (req,res) => {
@@ -8,6 +8,10 @@ router.get('/secret/:userId', requireLogin, isAuth, (req,res) => {
         user: req.profile
     })
 })
+
+router.get('/:userId', requireLogin, isAuth, getProfile)
+router.put('/:userId', requireLogin, isAuth, updateProfile)
+
 router.param('userId', userById)
 
 module.exports = router
