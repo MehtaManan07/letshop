65816e71cf580ce3d1dead67628ec7c0cf02f690
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-07-10 23:44:36 +0530

    Admin dashboard

---
+++ client/src/pages/admin/AdminDashBoard.js
@@ -0,0 +1,86 @@
+import React from "react";
+import Layout from "../../components/Layout";
+import { isAuth } from "../../functions/auth";
+import { Link } from "react-router-dom";
+const AdminDashboard = () => {
+  const {
+    data: {
+      user: { name, email, role },
+    },
+  } = isAuth();
+
+  const userInfo = () => (
+    <div className="card mb-5">
+      <h3 className="card-header"> User Information </h3>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col"> # </th>
+            <th scope="col"> Fields </th>
+            <th scope="col"> Data </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row"> 1 </th>
+            <th scope="row"> Name </th>
+            <th scope="row"> {name} </th>
+          </tr>
+          <tr>
+            <th scope="row"> 2 </th>
+            <th scope="row"> Email </th>
+            <th scope="row"> {email} </th>
+          </tr>
+          <tr>
+            <th scope="row"> 3 </th>
+            <th scope="row"> Role </th>
+            <th scope="row"> {role === 1 ? "Admin" : "Registered User"} </th>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+
+  const adminLinks = () => {
+    return (
+      <div className="card">
+        <h3 className="card-header"> Admin Links </h3>
+        <ul className="list-group">
+          <Link className="nav-link" to="/create/category">
+            Create Category
+          </Link>
+          <Link className="nav-link" to="/create/product">
+            Create Product
+          </Link>
+        </ul>
+      </div>
+    );
+  };
+
+  const ordersHistory = () => {
+    return (
+      <div className="card mb-5">
+        <h3 className="card-header"> Purchase History </h3>
+        <li className="list-group-item"> Role </li>
+      </div>
+    );
+  };
+
+  return (
+    <Layout
+      title="User Dashboard"
+      className="container"
+      description={`Hello ${name}`}
+    >
+      <div className="row">
+        <div className="col-3"> {userLinks()} </div>
+        <div className="col-9">
+          {userInfo()}
+          {ordersHistory()}
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default AdminDashboard;
