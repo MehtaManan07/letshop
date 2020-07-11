b5c504c0fc7c936ce68e50aba0248ebdd8d807a4
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-07-11 19:15:05 +0530

    getting value of category in checkbox

--- client/src/components/Checkbox.js
+++ client/src/components/Checkbox.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Checkbox = ({ categories }) => {
+  const [checked, setChecked] = useState([]);
+  
+  const handleToggle = (categoryId) => () => {
+    //return the first index or -1
+    const currentCategoryId = checked.indexOf(categoryId);
+    const newCheckedCategoryId = [...checked];
+    if (currentCategoryId === -1) {
+      newCheckedCategoryId.push(categoryId);
+    } else {
+      newCheckedCategoryId.splice(currentCategoryId, 1);
+    }
+    console.log(newCheckedCategoryId)
+    setChecked(newCheckedCategoryId)
+    console.log(checked)
+  };
+
   return categories.map((category) => (
     <li key={category._id} className="list-unstyled">
-      <input type="checkbox" className="form-check-input" />
+      <input
+        onChange={handleToggle(category._id)}
+        value={checked.indexOf(category._id === -1)}
+        type="checkbox"
+        className="form-check-input"
+      />
       <label className="form-check-label"> {category.name} </label>
     </li>
   ));
 };
+
 
 export default Checkbox;
