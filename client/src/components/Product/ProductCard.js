ed58a221017e775f9f645aa397104096978ae0df
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-07-13 10:27:48 +0530

    minor naming change

--- client/src/components/Product/ProductCard.js
+++ client/src/components/Product/ProductCard.js
@@ -8,7 +8,7 @@ const ProductCard = ({ product, showVViewButton = true }) => {
     <div className="card">
       <h3 className="card-header"> {product.name} </h3>
       <div className="card-body">
-        <ProductCardImage item={product} url="product" />
+        <ProductCardImage product={product} url="product" />
         <p className="lead mt-2"> {product.description.split(" ").slice(0, 10).join(" ")} </p>
         <p className="black-9"> $ {product.price} </p>
         <p className="black-7"> Category: <strong> { product.category && product.category.name} </strong> </p>
--- client/src/components/Product/ProductCardImage.js
+++ client/src/components/Product/ProductCardImage.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { API } from "../../config";
 
-const ProductCardImage = ({ item, url }) => {
+const ProductCardImage = ({ product, url }) => {
   return (
     <div className="product-img">
       <img
-        src={`${API}/${url}/picture/${item._id}`}
-        alt={item.name}
+        src={`${API}/${url}/picture/${product._id}`}
+        alt={product.name}
         style={{ maxHeight: "100%", maxWidth: "100%" }}
         className="mb-3"
       />
