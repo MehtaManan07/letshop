197f9cf74a664033a03dce0a9a84e9449056a1e6
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-07-14 18:45:18 +0530

    added route for payment

--- routes/braintree.js
+++ routes/braintree.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router()
 const { requireLogin, isAuth } = require('../controllers/Auth');
 const { userById } = require('../controllers/User');
-const { generateToken } = require('../controllers/Payment')
+const { generateToken, processPayment } = require('../controllers/Payment')
 
 router.get('/braintree/getToken/:userId', requireLogin, isAuth, generateToken)
+router.post('/braintree/payment/:userId', requireLogin, isAuth, processPayment)
 
 router.param('userId', userById)
 
